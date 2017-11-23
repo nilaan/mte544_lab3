501e869fc23b63ba93a9f6d272a47cab4a28884f
nilaan <g.nilaan@gmail.com>
2017-11-23 17:55:16 -0500

    add bresenham

--- src/turtlebot_example.cpp
+++ src/turtlebot_example.cpp
@@ -6,6 +6,7 @@
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <stdlib.h>
 
 ros::Publisher marker_pub;
 visualization_msgs::Marker points;
@@ -19,6 +20,9 @@ uint8_t foundMap = 0;
 uint16_t numNodes = 500;
 double *nodesX;
 double *nodesY;
+
+uint16_t numEdges = 20;
+uint8_t *connectedEdges;
 
 double offsetX = 1.0;
 double offsetY = 5.0;
@@ -35,12 +39,102 @@ double initYaw = NAN;
 
 
 #define TAGID 0
+/*
+double sgn(double x) { return x >= 0 ? 1.0 : -1.0; }
+
+void bresenham(double x0, double y0, double x1, double y1, std::vector<double>& x, std::vector<double>& y)
+{
+
+	double dx = fabs(x1-x0);
+	double dy = fabs(y1-y0);
+	double dx2 = x1-x0;
+	double dy2 = y1-y0;
+
+	const bool s = fabs(dy) > abs(dx);
+
+	if (s)
+	{
+		dx2 = dx;
+		dx = dy;
+		dy = dx2;
+	}
+
+	double inc1 = 2*dy;
+	double d = inc1-dx;
+	double inc2 = d-dx;
+
+	x.push_back(x0);
+	y.push_back(y0);
+
+
+	for (int i = 0; i < 10; i++);
+	{
+		if (s) 
+			y0 += sgn(dy2); 
+		else 
+			x0 += sgn(dx2);
+		if (d < 0)
+			d += inc1;
+		else
+		{
+			d += inc2;
+			if (s)
+				x0 += sgn(dx2);
+			else
+				y0 += sgn(dy2);
+		}
+		ROS_INFO("%f %f", x0, y0);
+		x.push_back(x0);
+		y.push_back(y0);
+	}
+
+
+}
+
+*/
+short sgn(int x) { return x >= 0 ? 1 : -1; }
+
+void bresenham(int x0, int y0, int x1, int y1, std::vector<int>& x, std::vector<int>& y) {
+
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int dx2 = x1 - x0;
+    int dy2 = y1 - y0;
+    
+    const bool s = abs(dy) > abs(dx);
+
+    if (s) {
+        int dx2 = dx;
+        dx = dy;
+        dy = dx2;
+    }
+
+    int inc1 = 2 * dy;
+    int d = inc1 - dx;
+    int inc2 = d - dx;
+
+    x.push_back(x0);
+    y.push_back(y0);
+
+    while (x0 != x1 || y0 != y1) {
+        if (s) y0+=sgn(dy2); else x0+=sgn(dx2);
+        if (d < 0) d += inc1;
+        else {
+            d += inc2;
+            if (s) x0+=sgn(dx2); else y0+=sgn(dy2);
+        }
+
+        //Add point to vector
+        x.push_back(x0);
+        y.push_back(y0);
+    }
+}
 
 void plotPoints()
 {
 	geometry_msgs::Point p;
 	points.header.frame_id = "/map";
-	points.id = 0; //each curve must have a unique id or you will overwrite an old ones
+	points.id = 0;	 //each curve must have a unique id or you will overwrite an old ones
 	points.type = visualization_msgs::Marker::POINTS;
 	points.action = visualization_msgs::Marker::ADD;
 	points.ns = "points_and_lines";
@@ -57,6 +151,42 @@ void plotPoints()
 		points.points.push_back(p);
 	}
 	marker_pub.publish(points);
+}
+
+void plotLines()
+{
+	geometry_msgs::Point p;
+	double slope;
+	points.header.frame_id = "/map";
+	points.type = visualization_msgs::Marker::LINE_STRIP;
+	points.action = visualization_msgs::Marker::ADD;
+	points.ns = "points_and_lines";
+	points.scale.x = 0.1;
+	points.scale.y = 0.1;
+	points.color.b = 1.0;
+	points.color.a = 1.0;
+
+	for(int i = 0; i < numNodes; i++)
+	{
+		for(int j = 0; j < numNodes; j++)
+		{
+			if (connectedEdges[i*mapWidth+j] == 1)
+			{
+				points.points.clear();
+				p.x = nodesX[i];
+				p.y = nodesY[i];
+				p.z = 0.0;
+				points.points.push_back(p);
+
+				p.x = nodesX[j];
+				p.y = nodesY[j];
+				p.z = 0.0;
+				points.points.push_back(p);
+				marker_pub.publish(points);
+			}
+		}
+	}
+
 }
 
 void generateNodes()
@@ -95,18 +225,119 @@ void removeCollisionNodes()
 	nodesX = new double[numNodes];
 	nodesY = new double[numNodes];
 
-	nodesX[0] = initX+offsetX;
-	nodesY[0] = initY+offsetY;
-	nodesX[1] = finalX+offsetX;
-	nodesY[1] = finalY+offsetY;
-
-	for (int i = 2; i < count; i++)
+	for (int i = 0; i < count; i++)
 	{
 		nodesX[i] = tempX[i];
 		nodesY[i] = tempY[i];
 	}
 
+	nodesX[count] = initX+offsetX;
+	nodesY[count] = initY+offsetY;
+	nodesX[count+1] = finalX+offsetX;
+	nodesY[count+1] = finalY+offsetY;
+
 	ROS_INFO("Number of nodes kept %d.", numNodes);
+}
+
+double getNormDist(int i, int j)
+{
+	return sqrt((nodesX[j]-nodesX[i])*(nodesX[j]-nodesX[i])+(nodesY[j]-nodesY[i])*(nodesY[j]-nodesY[i]));
+}
+
+void sort(double arr[], int ind[], int left, int right)
+{
+	int i = left;
+	int j = right; 
+	int tmpInd;
+	double tmp;
+	double pivot = arr[(left+right)/2];
+
+	while (i <= j)
+	{
+		while (arr[i] < pivot)
+			i++;
+		while (arr[j] > pivot)
+			j--;
+		if (i <= j)
+		{
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+
+			tmpInd = ind[i];
+			ind[i] = ind[j];
+			ind[j] = tmpInd;
+			i++;
+			j--;
+		}
+	}
+
+	if (left < j)
+		sort(arr, ind, left, j);
+	if (i < right)
+		sort(arr, ind, i, right);
+}
+
+int8_t checkCollision(int i, int j)
+{
+	int x0 = nodesX[i]/mapRes;
+	int x1 = nodesX[j]/mapRes;
+	int y0 = nodesY[i]/mapRes;
+	int y1 = nodesY[j]/mapRes;
+	uint16_t ind = 0;
+	long int size = 0;
+	std::vector<int> indX;
+	std::vector<int> indY;
+
+	ROS_INFO("%d %d %d %d", x0, y0, x1, y1);
+	bresenham(x0, y0, x1, y0, indX, indY);
+
+	size = indX.size();
+	ROS_INFO("%d", size);
+
+	for (int k = 0; k < size; k++)
+	{
+		ind = indY[k]*mapHeight+indX[k];
+		if (mapData[ind])
+		{
+			ROS_INFO("Edge Collides %d %d", ind, mapData[ind]);
+			return 1;
+		}
+		ROS_INFO("%d %d", ind, mapData[ind]);
+	}
+	ROS_INFO("Edge Did Not Collide");
+
+	return 0;
+
+}
+
+void connectEdges()
+{
+	int i = 0;
+	int j = 0;
+	double dists[numNodes];
+	int ind[numNodes];
+	connectedEdges = new uint8_t[numNodes*numNodes];
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < numNodes; j++)
+		{
+			dists[j] = getNormDist(i, j);
+			ind[j] = j;
+		}
+		sort(dists, ind, 0, numNodes-1);
+		//ROS_INFO("%d %f %f %f %f %f", i, dists[0], dists[1], dists[2], dists[3], dists[4]);
+		for (j = 1; j <= numEdges; j++)
+		{
+			ROS_INFO("%d %d", i, ind[j]);
+			if(!checkCollision(i, ind[j]))
+			{
+				connectedEdges[i*mapWidth+ind[j]] = 1;
+				//connectedEdges[ind[j]*mapHeight+i] = 1;
+			}
+		}
+	}
+	ROS_INFO("Connected Closest Edges & Removed Collisions");
 }
 
 
@@ -250,7 +481,9 @@ int main(int argc, char **argv)
 	{
 		removeCollisionNodes();
 		plotPoints();
+		connectEdges();
 		foundMap++;
+		plotLines();
 	}
 
 
