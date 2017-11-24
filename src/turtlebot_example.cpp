d9906244c18d4de1cd1652a306a6692a2b28b1a6
nilaan <g.nilaan@gmail.com>
2017-11-24 01:23:15 -0500

    fixing bugs.

--- src/turtlebot_example.cpp
+++ src/turtlebot_example.cpp
@@ -21,7 +21,7 @@ uint16_t numNodes = 500;
 double *nodesX;
 double *nodesY;
 
-uint16_t numEdges = 20;
+uint16_t numEdges = 30;
 uint8_t *connectedEdges;
 
 double offsetX = 1.0;
@@ -39,62 +39,10 @@ double initYaw = NAN;
 
 
 #define TAGID 0
-/*
-double sgn(double x) { return x >= 0 ? 1.0 : -1.0; }
 
-void bresenham(double x0, double y0, double x1, double y1, std::vector<double>& x, std::vector<double>& y)
-{
-
-	double dx = fabs(x1-x0);
-	double dy = fabs(y1-y0);
-	double dx2 = x1-x0;
-	double dy2 = y1-y0;
-
-	const bool s = fabs(dy) > abs(dx);
-
-	if (s)
-	{
-		dx2 = dx;
-		dx = dy;
-		dy = dx2;
-	}
-
-	double inc1 = 2*dy;
-	double d = inc1-dx;
-	double inc2 = d-dx;
-
-	x.push_back(x0);
-	y.push_back(y0);
-
-
-	for (int i = 0; i < 10; i++);
-	{
-		if (s) 
-			y0 += sgn(dy2); 
-		else 
-			x0 += sgn(dx2);
-		if (d < 0)
-			d += inc1;
-		else
-		{
-			d += inc2;
-			if (s)
-				x0 += sgn(dx2);
-			else
-				y0 += sgn(dy2);
-		}
-		ROS_INFO("%f %f", x0, y0);
-		x.push_back(x0);
-		y.push_back(y0);
-	}
-
-
-}
-
-*/
 short sgn(int x) { return x >= 0 ? 1 : -1; }
 
-void bresenham(int x0, int y0, int x1, int y1, std::vector<int>& x, std::vector<int>& y) {
+void bresenham(int x0, int y0, int x1, int y1, std::vector<int>& indX, std::vector<int>& indY) {
 
     int dx = abs(x1 - x0);
     int dy = abs(y1 - y0);
@@ -113,8 +61,8 @@ void bresenham(int x0, int y0, int x1, int y1, std::vector<int>& x, std::vector<
     int d = inc1 - dx;
     int inc2 = d - dx;
 
-    x.push_back(x0);
-    y.push_back(y0);
+    indX.push_back(x0);
+    indY.push_back(y0);
 
     while (x0 != x1 || y0 != y1) {
         if (s) y0+=sgn(dy2); else x0+=sgn(dx2);
@@ -125,8 +73,8 @@ void bresenham(int x0, int y0, int x1, int y1, std::vector<int>& x, std::vector<
         }
 
         //Add point to vector
-        x.push_back(x0);
-        y.push_back(y0);
+        indX.push_back(x0);
+        indY.push_back(y0);
     }
 }
 
@@ -268,6 +216,7 @@ void sort(double arr[], int ind[], int left, int right)
 			tmpInd = ind[i];
 			ind[i] = ind[j];
 			ind[j] = tmpInd;
+
 			i++;
 			j--;
 		}
@@ -281,32 +230,32 @@ void sort(double arr[], int ind[], int left, int right)
 
 int8_t checkCollision(int i, int j)
 {
-	int x0 = nodesX[i]/mapRes;
-	int x1 = nodesX[j]/mapRes;
-	int y0 = nodesY[i]/mapRes;
-	int y1 = nodesY[j]/mapRes;
-	uint16_t ind = 0;
+	int x0 = (int)(nodesX[i]/mapRes);
+	int x1 = (int)(nodesX[j]/mapRes);
+	int y0 = (int)(nodesY[i]/mapRes);
+	int y1 = (int)(nodesY[j]/mapRes);
+	int ind;
 	long int size = 0;
 	std::vector<int> indX;
 	std::vector<int> indY;
 
-	ROS_INFO("%d %d %d %d", x0, y0, x1, y1);
-	bresenham(x0, y0, x1, y0, indX, indY);
+//	ROS_INFO("%d %d %d %d", x0, y0, x1, y1);
+	bresenham(x0, y0, x1, y1, indX, indY);
 
 	size = indX.size();
-	ROS_INFO("%d", size);
+//	ROS_INFO("%d", size);
 
 	for (int k = 0; k < size; k++)
 	{
 		ind = indY[k]*mapHeight+indX[k];
-		if (mapData[ind])
+		if (mapData[ind] == 100)
 		{
-			ROS_INFO("Edge Collides %d %d", ind, mapData[ind]);
+//			ROS_INFO("Edge Collides %d %d", ind, mapData[ind]);
 			return 1;
 		}
-		ROS_INFO("%d %d", ind, mapData[ind]);
+//		ROS_INFO("%d %d", ind, mapData[ind]);
 	}
-	ROS_INFO("Edge Did Not Collide");
+//	ROS_INFO("Edge Did Not Collide");
 
 	return 0;
 
@@ -330,11 +279,16 @@ void connectEdges()
 		//ROS_INFO("%d %f %f %f %f %f", i, dists[0], dists[1], dists[2], dists[3], dists[4]);
 		for (j = 1; j <= numEdges; j++)
 		{
-			ROS_INFO("%d %d", i, ind[j]);
+//			ROS_INFO("%d %d", i, ind[j]);
 			if(!checkCollision(i, ind[j]))
 			{
 				connectedEdges[i*numNodes+ind[j]] = 1;
-				//connectedEdges[ind[j]*mapHeight+i] = 1;
+				connectedEdges[ind[j]*numNodes+i] = 1;
+			}
+			else
+			{
+				connectedEdges[i*numNodes+ind[j]] = 0;
+				connectedEdges[ind[j]*numNodes+i] = 0;
 			}
 		}
 	}
